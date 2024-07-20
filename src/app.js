29835a10e3fd85ba85b50f73faa25c8a29339b2e
mamel210 <mamel210@gmail.com>
2024-07-20 00:41:33 +0000

    generator

--- src/app.js
+++ src/app.js
@@ -5,7 +5,23 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function () {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ['the', 'our', 'my'];
+  let adj = ['great', 'big'];
+  let noun = ['jogger', 'racoon'];
+  let tarjeta = ''
+  for (const iterator of pronoun) {
+    for (const iterAtor of adj) {
+      for (const iterat0r of noun) {
+        let texto = iterator + iterAtor + iterat0r + '.com'
+        console.log(texto)
+        tarjeta += "<li> "+ texto +"</li>"
+      }
+    }
+  }
+
+  const cardHTML = document.querySelector('#domined');
+  cardHTML.innerHTML = tarjeta
+  
 };
