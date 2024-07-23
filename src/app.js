36626244e469be210cb4c2cbb053c20f3486b818
mamel210 <mamel210@gmail.com>
2024-07-23 00:30:41 +0000

    fix: apply code style

--- src/app.js
+++ src/app.js
@@ -1,27 +1,39 @@
 /* eslint-disable */
 import "bootstrap";
 import "./style.css";
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+
 window.onload = function () {
-  //write your code here
+  // Write your code here
   let pronoun = ['the', 'our', 'my'];
-  let adj = ['great', 'big'];
+  let adjective = ['great', 'big'];
   let noun = ['jogger', 'racoon'];
-  let tarjeta = ''
+  let webTextItem = ''
   for (const iterator of pronoun) {
-    for (const iterAtor of adj) {
-      for (const iterat0r of noun) {
-        let texto = iterator + iterAtor + iterat0r + '.com'
-        console.log(texto)
-        tarjeta += "<li> "+ texto +"</li>"
+    for (const iterator1 of adjective) {
+      for (const iterator2 of noun) {
+        let texto = iterator + iterator1 + iterator2 + '.com'
+        webTextItem += "<li class='list-group-item bg-warning p-1 ms-2'> " + texto + "</li>"
       }
     }
   }
-
-  const cardHTML = document.querySelector('#domined');
-  cardHTML.innerHTML = tarjeta
-  
+  const webText = document.querySelector('#domined');
+  webText.innerHTML = webTextItem
+  // Colum 2
+  let names = ['jose', 'daclerico', 'douglas'];
+  let lastNames = ['ramirez', 'mata'];
+  let ages = ['42', '38']
+  let fullNameItem = ''
+  names.map((name) => {
+    lastNames.map((lastName) => {
+      ages.map((age) => {
+        const text = name + lastName + age + '.net'
+        fullNameItem += "<li class='list-group-item p-1 ms-2 bg-info'> " + text + "</li>"
+      })
+    })
+ })
+  const fullNameItemText = document.querySelector('#fullName');
+  fullNameItemText.innerHTML = fullNameItem;
 };
